b0ffa9f706e45c8369cfebec9f425ebf9add64f1
EduardoAjcajon <eaajcajon@scl.edu.gt>
2024-03-06 13:41:16 -0600

    menu

---
+++ js/perfil_component.js
@@ -0,0 +1,12 @@
+let perfil = `
+    <div class="nombre_usuario  ">
+        <div class="logo">
+            RM
+        </div>
+        <span>
+            EduardoAjcajon  
+        </span>
+    </div>
+`;
+
+export {perfil}
