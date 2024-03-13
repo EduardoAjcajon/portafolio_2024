c65fa8c5aeae7941bb1983f63d75b305d739aa90
EduardoAjcajon <eaajcajon@scl.edu.gt>
2024-03-13 13:38:36 -0600

    diseño de paginas destacada

---
+++ js/data_web.js
@@ -0,0 +1,25 @@
+let lista_webs = [
+    {
+        nombre: "Juego de memoria",
+        img: "https://raw.githubusercontent.com/EduardoAjcajon/img-s/main/imgs/juego_de_cartas.png?token=GHSAT0AAAAAACOP62F7D7ISRQUBMUB4O5ZGZPR74MA",
+        link_repo: "https://github.com/EduardoAjcajon/Juego_de_memoria_2024",
+        github_page: "https://eduardoajcajon.github.io/Juego_de_memoria_2024/",
+        descripcion: "Esta es una descripcion"
+    },
+    {
+        nombre: "Calculadora",
+        img: "https://raw.githubusercontent.com/EduardoAjcajon/img-s/main/imgs/calculadora.png?token=GHSAT0AAAAAACOP62F62B2KQQIC754VGRMYZPR75MQ",
+        link_repo: "",
+        github_page: "",
+        descripcion: "Esta es una descripcion"
+    },
+    {
+        nombre: "Dom y eventos de js",
+        img: "https://raw.githubusercontent.com/EduardoAjcajon/img-s/main/imgs/dom_y_evento_js.png?token=GHSAT0AAAAAACOP62F6YWHLFSCFA5WSOCF6ZPR753A",
+        link_repo: "https://github.com/EduardoAjcajon/Dom_y_eventos_de_js?tab=readme-ov-file",
+        github_page: "https://eduardoajcajon.github.io/Dom_y_eventos_de_js/",
+        descripcion: "Esta es una descripcion"
+    },
+];
+
+export { lista_webs };
